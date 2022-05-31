d74f346e37a4bd6a501c18209e23f20eae587669
TigranWeb <tigrank.arm@gmail.com>
2022-05-31 22:24:32 +0400

    test

---
+++ build/precache-manifest.99e50bb9ee48e2c3975f09ec4f0774dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6261ce061b751bd7cf6",
+    "url": "./static/css/main.52eb7a1e.chunk.css"
+  },
+  {
+    "revision": "f6261ce061b751bd7cf6",
+    "url": "./static/js/main.f6261ce0.chunk.js"
+  },
+  {
+    "revision": "0728f01e23e0f4f951c0",
+    "url": "./static/js/1.0728f01e.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6a0f83f417665e19ad073baecc7c5bf6",
+    "url": "./index.html"
+  }
+];
