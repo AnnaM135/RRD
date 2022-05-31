75ddcd13aa104fea2aa2f9b7fb9215b491798d9c
TigranWeb <tigrank.arm@gmail.com>
2022-05-31 22:45:39 +0400

    test6

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const app = express();
 
 const path = require("path");
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+const port = 5000;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  console.log("sssssssssss");
+  app.use(express.static("build/"));
   app.get("*", (r, q) => {
-    r.sendFile(path.resolve(__dirname, "build", index.html));
+    r.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
+// console.log("sssssssssss");
+// app.use(express.static("build/"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "build", "index.html"));
+// });
 app.listen(port, (err) => {
   if (err) return console.log(err);
   console.log("server is run");
